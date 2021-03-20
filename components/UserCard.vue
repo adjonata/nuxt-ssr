e081739ccd8fc917aaf50cd469e880a4962fc8ec
adjonata <alexdjonata30@gmail.com>
2021-03-20 17:23:06 -0300

    feat: nuxt ssr

---
+++ components/UserCard.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="user-card" v-if="user">
+    <img src="http://placehold.it/92x92" :alt="user.name" />
+    <div class="user-card__content">
+      <h3>{{ user.name }}</h3>
+      <p>{{ user.about }}</p>
+      <button @click="$emit('remove', user.id)">Remover</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.user-card {
+  background: #f1f1f1;
+  box-shadow: 0 2px 3px 0 rgba(39, 39, 39, 0.116);
+  padding: 15px;
+  border-radius: 10px;
+  margin: 15px 0;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+.user-card img {
+  margin-top: 15px;
+  border-radius: 7px;
+}
+.user-card__content {
+  padding: 15px;
+  color: #4d4d4d;
+}
+.user-card__content h3 {
+  font-size: 16px;
+}
+.user-card__content p {
+  font-size: 13px;
+  margin-top: 5px;
+}
+.user-card button {
+  margin-top: 10px;
+  cursor: pointer;
+  padding: 10px;
+  border-radius: 7px;
+  border: 0;
+  color: #f1f1f1;
+  background: rgb(212, 55, 63);
+  transition: filter 0.3s;
+  box-shadow: 0 2px 3px 0 rgba(39, 39, 39, 0.116);
+}
+.user-card button:hover {
+  filter: brightness(90%);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <div class="container-content">
+      <UserCard
+        v-for="user in users"
+        :key="user.id"
+        :user="user"
+        @remove="removeUser"
+      />
+    </div>
+    <div class="container-count">{{ users.length }} Usuários</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data: () => ({
+    users: [],
+  }),
+  async fetch() {
+    this.users = await fetch('https://adjonata.github.io/json/users.json')
+      .then((response) => response.json())
+      .catch((error) => console.error(error))
+  },
+  fetchOnServer: true,
+  methods: {
+    removeUser(index) {
+      this.users.splice(index, 1)
+    },
+  },
+}
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  outline: none;
+  background: #f1f1f1;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.container-content {
+  width: 600px;
+  padding: 30px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.container-count {
+  position: fixed;
+  top: 35px;
+  right: 35px;
+  color: #353535;
+  font-weight: bold;
+}
+</style>
